3da4f1515ed046246681754035a9e00f56dd91c3
Heybooka <ebuka@localhost.localdomain>
2019-05-31 10:02:13 +0100

    Date

---
+++ DateTest.java
@@ -0,0 +1,13 @@
+// Test class for Date
+// Author: CHIMA CHUKWUEBUKA
+// Date: 31st May 2019
+
+public class DateTest{
+
+
+    public static void main(String[] args){
+        Date one = new Date(23, 12, 2019);
+
+        System.out.println("SaintSamiGanja Today is: " + one.displayDate());
+    }
+}
